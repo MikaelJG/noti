d6b80a370b51cd23e7bb5efe9c4a7c732218f4e7
Mikael J. Gonsalves <mjgonsalves@protonmail.com>
2022-12-31 13:07:22 -0500

    cleaner write it function

--- writeit/writeit.sh
+++ writeit/writeit.sh
@@ -5,40 +5,43 @@ LANGUAGES=( "js" "js" "react" "rb" "bash" "sh" "html" "rs" "ts" "css" "cs" )
 
 # find the ex file
 
-if [[ "${LANGUAGES[*]}" =~ " $1 " ]];
-    then
+FIND_LANGUAGE_NOTES() {
     
-    MANUAL="$1"man
-
-    DIR=~/code/myman/${MANUAL}
-
-    for file in "$DIR"/*
+DIR=~/code/noti/$1
+    
+for file in "$DIR"/*
     do
         if [ -f "$file" ];
             then
-
             filename=$(basename -- "$file")
             extension="${filename##*.}"
             filename="${filename%.*}"
-
+            
             FILES_WITH_NOTES_ARRAY+=( "$filename" )
-        fi
-    done
+       fi
+done
+}
+
+MOD_LAST_FILE() {
 
     if [[ "${FILES_WITH_NOTES_ARRAY[*]}" =~ " $2 " ]];
         then
         
         LAST_MODIFIED_FILE=$(ls -Art | tail -n 1)
 
-        cat ~/code/myman/${MANUAL}/$2.txt >> $LAST_MODIFIED_FILE
+        cat ~/code/noti/${MANUAL}/$2.txt >> $LAST_MODIFIED_FILE
 
     else
         echo "no file with this name"
     fi
+}
 
+if [[ "${LANGUAGES[*]}" =~ " $1 " ]];
+    then
+    FIND_LANGUAGE_NOTES
+    MOD_LAST_FILE
 else 
     echo "language not supported"
-
 fi
 
 
