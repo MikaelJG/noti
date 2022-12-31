ea2dc8c5107719ad7617d969c802b8638b1b884a
Mikael J. Gonsalves <mjgonsalves@protonmail.com>
2022-12-31 13:43:23 -0500

    rename

---
+++ noti.sh
@@ -0,0 +1,61 @@
+# append this example to my file. 
+
+FILES_WITH_NOTES_ARRAY=()
+LANGUAGES=( "js" "js" "re" "ht" "rb" "sh" "rs" "ts" "css" "cs" )
+
+# find the ex file
+
+FIND_READ_LANGUAGE_NOTES() {
+    
+    DIR=~/code/noti/$1
+    
+    for file in "$DIR"/*
+        do
+            if [ -f "$file" ];
+                then
+                filename=$(basename -- "$file")
+                extension="${filename##*.}"
+                filename="${filename%.*}"
+                
+                LANGUAGE_NOTES_ARRAY+=( "$filename" )
+           fi
+    done
+
+    if [[ "${LANGUAGE_NOTES_ARRAY[*]}" =~ " $2 " ]];
+        then
+            echo ""
+            cat ~/code/noti/$1/$2
+            echo ""
+    else
+        echo "File does not exist"
+    fi
+}
+
+MOD_LAST_FILE() {
+
+    if [[ "${LANGUAGES[*]}" =~ " $2 " ]] && [[ "${FILES_WITH_NOTES_ARRAY[*]}" =~ " $3 " ]];
+        then
+        
+        LAST_MODIFIED_FILE=$(ls -Art | tail -n 1)
+
+        cat ~/code/noti/$1/$2.txt >> $LAST_MODIFIED_FILE
+    else
+        echo "Language or file is not supported"
+    fi
+}
+
+if [[ "$1" == "-w" ]];
+    then
+    FIND_READ_LANGUAGE_NOTES
+    MOD_LAST_FILE
+
+elif [[ "${LANGUAGES[*]}" =~ " $1 " ]];
+    then
+    FIND_READ_LANGUAGE_NOTES
+else 
+    echo "language not supported"
+fi
+
+
+
+
