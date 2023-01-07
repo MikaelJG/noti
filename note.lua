dcc45c037aa8f7e6c6b4bf900c706a814d2e3164
MikaelJG <mjgonsalves@protonmail.com>
2023-01-07 14:31:16 -0500

    languages in lua

--- note.lua
+++ note.lua
@@ -1,4 +1,39 @@
-print("hello from lua")
+-- args saved in table arg
 
--- this is a comment
+print(table.concat(arg,", "))
 
+for i, v in ipairs(arg) do
+    print(v)
+end
+
+-- languages on computer
+
+local languages = {
+    -- value = key
+    "js" = "javascript"
+    "sh" = "bash"
+    "css" = "css"
+    "rx" = "regex"
+    "sql" = "sql"
+    "rs" = "rust"
+    "ts" = "typescript"
+    "cs" = "csharp"
+    "go" = "golang"
+    "ph" = "php"
+    "ra" = "rails"
+    "th" = "three.js"
+    "vi" = "vim"
+    "gi" = "git"
+    "lu" = "lua"
+    "no" = "node.js"
+    "py" = "python"
+    "ht" = "html"
+    "rb" = "ruby"
+    "re" = "react"
+}
+
+-- command should be noti -n re name-of-file  
+
+-- move to directory of folder
+-- open vim name-of-file  
+-- when vim done, exit
