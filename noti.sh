80cef46c604e50805b36ceea5419fd8006b3e2d8
MikaelJG <mjgonsalves@protonmail.com>
2023-01-12 16:11:07 -0500

    added folder r

--- noti.sh
+++ noti.sh
@@ -6,7 +6,7 @@ declare -A LANGUAGES
 NOTI_DIRECTORY=~/code/noti # DEFINE NOTI_DIRECTORY, ex: ~/path/to/noti
 NOTES_ARRAY=()
 LANGUAGE_ARRAY=()
-LANGUAGES=( ["js"]="javascript" ["tm"]="tmux" ["rx"]="regex" ["sql"]="sql" ["js"]="javascript" ["re"]="react" ["ht"]="html" ["rb"]="ruby" ["sh"]="bash" ["rs"]="rust" ["ts"]="typescript" ["css"]="css" ["cs"]="csharp" ["go"]="golang" ["ph"]="php" ["py"]="python" ["ra"]="rails" ["th"]="three.js" ["vi"]="vim" ["gi"]="git" ["lu"]="lua" ["no"]="node.js" ["sa"]="sass" ["mo"]="mongodb" )
+LANGUAGES=( ["js"]="javascript" ["r"]="r" ["tm"]="tmux" ["rx"]="regex" ["sql"]="sql" ["js"]="javascript" ["re"]="react" ["ht"]="html" ["rb"]="ruby" ["sh"]="bash" ["rs"]="rust" ["ts"]="typescript" ["css"]="css" ["cs"]="csharp" ["go"]="golang" ["ph"]="php" ["py"]="python" ["ra"]="rails" ["th"]="three.js" ["vi"]="vim" ["gi"]="git" ["lu"]="lua" ["no"]="node.js" ["sa"]="sass" ["mo"]="mongodb" )
 
 FIND_NOTES() {
     DIR="${NOTI_DIRECTORY}/languages/$1"
