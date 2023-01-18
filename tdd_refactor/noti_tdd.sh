35d177d0ba76a4af670724b482ff726877efce59
MikaelJG <mjgonsalves@protonmail.com>
2023-01-18 16:36:56 -0500

    command option

--- tdd_refactor/noti_tdd.sh
+++ tdd_refactor/noti_tdd.sh
@@ -7,17 +7,18 @@ source variables.sh
 . ./mod_last_file.sh
 
 FIND_LANGUAGES
-FIND_NOTES
-# echo "${NOTES_ARRAY[@]}"
-# echo "${LANGUAGE_ARRAY[@]}"
-
+FIND_NOTES $1
 # if [[ "$1" == "-w" ]];
 #     then
 #     FIND_NOTES $2
 #     MOD_LAST_FILE $2 $3
 if [[ "$1" == "option" ]] || [[ "$1" == "options" ]] || [[ "$1" == "command" ]] || [[ "$1" == "command" ]];
      then
-     echo "in options"
+     echo "
+     noti -l : list all supported languages
+     noti rs ls : list all notes in rust
+     noti rs ls : list all notes in rust
+     "
 elif [[ "$1" == "-l" ]];
      then
          echo "Supported languages: "
@@ -25,10 +26,13 @@ elif [[ "$1" == "-l" ]];
          do
              echo "$i - ${LANGUAGES[$i]}";
          done
-elif [[ "${LANGUAGE_ARRAY[*]}" =~ " $1 " ]];
+elif [[ "${LANGUAGE_ARRAY[*]}" =~ " $1 " ]] && [[ "$2" != "ls" ]];
      then
      FIND_NOTES $1
      READ_NOTES $1 $2
- else
+elif [[ "${LANGUAGE_ARRAY[*]}" =~ " $1 " ]] && [[ "$2" == "ls" ]];
+    then
+    echo "${NOTES_ARRAY[@]}"
+else
      echo "language not supported"
 fi
