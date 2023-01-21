b72f6eeea7540eee9eeba37e77d2f0ad9bb3ac12
MikaelJG <mjgonsalves@protonmail.com>
2023-01-21 07:52:27 -0500

    refactoring lua

--- nit/languages.lua
+++ nit/languages.lua
@@ -1,8 +1,30 @@
 -- treat languages.lua like a module and import it to nit.lua via require
-
-local languages = 
-{
-    print("hello from languages.lua")
+local languages = {
+    -- value = key
+    ["js"] = "javascript",
+    ["sh"] = "bash",
+    ["css"] = "css",
+    ["rx"] = "regex",
+    ["r"] = "r",
+    ["sql"] = "sql",
+    ["rs"] = "rust",
+    ["ts"] = "typescript",
+    ["cs"] = "csharp",
+    ["go"] = "golang",
+    ["ph"] = "php",
+    ["ra"] = "rails",
+    ["th"] = "three.js",
+    ["vi"] = "vim",
+    ["gi"] = "git",
+    ["lu"] = "lua",
+    ["no"] = "node.js",
+    ["py"] = "python",
+    ["ht"] = "html",
+    ["rb"] = "ruby",
+    ["re"] = "react",
+    ["sa"] = "sass",
+    ["mo"] = "mongodb",
+    ["tm"] = "tmux",
 }
 
 return languages
