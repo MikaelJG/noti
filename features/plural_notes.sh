0a404e45f88439e5d69bf423753146f3cdb045a0
MikaelJG <mjgonsalves@protonmail.com>
2023-01-25 10:42:40 -0500

    cleaning

--- features/plural_notes.sh
+++
@@ -1,9 +0,0 @@
-# create all variants of a note with -s
-
-# if -s
-# if it doesn't exist already
-# cp last_file last_file + s
-
-# echo "New file ${last_file +s} created"
-
-
--- features/simplest.sh
+++
@@ -1,8 +0,0 @@
-# filter notes by complexity
-# low
-# mid
-# high (most complex example)
-
-# show me the simplest example
-# vs
-# show me a real life example.
