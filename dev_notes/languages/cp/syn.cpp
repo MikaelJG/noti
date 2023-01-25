155ab855baf1d7672e95fa5bb60212f4b2cc1f2f
MikaelJG <mjgonsalves@protonmail.com>
2023-01-25 13:07:52 -0500

    printing

---
+++ dev_notes/languages/cp/syn.cpp
@@ -0,0 +1,15 @@
+#include <iostream> //get user input
+#include <string> //work with strings
+using namespace std; //using standard namespace, no need to prefix all cpp
+
+int main() / code inside will be executed
+{
+    // printing
+
+    cout << "Hello" << endl; // console out
+    cout << "World";
+    cout << "!" << endl; // endl, print new line
+
+    return 0;
+}
+
--- dev_notes/languages/cp/var.cpp
+++ dev_notes/languages/cp/var.cpp
@@ -1,2 +1,11 @@
 #include <iostream> //get user input
 #include <string> //work with strings
+using namespace std; //using standard namespace, no need to prefix all cpp
+
+int main() / code inside will be executed
+{
+
+
+    return 0;
+}
+
