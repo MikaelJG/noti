b7ac835be7b9b29a0be7f8ead5c2ba7652386a0e
MikaelJG <mjgonsalves@protonmail.com>
2023-02-03 12:55:34 -0500

    cleaning cs

--- nit/nit.lua
+++ nit/nit.lua
@@ -4,10 +4,6 @@ local noti_dir = "~/code/noti"
 local languages = require 'languages' 
 local definitions = require 'definitions'
 
--- something about package.path = package.path .. ";./languages.lua"
--- making the search for require impossible
--- to verify
-
 first_arg = arg[1]
 second_arg = arg[2]
 
@@ -21,7 +17,6 @@ local function hash_has_value (tab, val)
     return false
 end
 local function hash_hasnot_value (tab, val)
-    -- for index, value
     for i, v in pairs(languages) do
         if i == val then
             return false 
@@ -29,16 +24,6 @@ local function hash_hasnot_value (tab, val)
     end
     return true 
 end
---local function array_has_value (tab, val)
---    -- for index, value
---    for i, v in ipairs(definitions) do
---        if v == val then
---            return true
---        end
---    end
---
---    return false
---end
 
 if hash_has_value(languages, first_arg) and second_arg ~= nil then
     dir = string.format("%s/languages/%s/%s", noti_dir, first_arg, second_arg) 
