1eafaa1a6ea6718df8a7ce05e6093c5e5180092c
MikaelJG <mjgonsalves@protonmail.com>
2023-02-14 14:31:45 -0500

    major refactoring

--- noti/find/find_all.sh
+++ noti/find/find_all.sh
@@ -14,62 +14,22 @@ LANGUAGES_D="${NOTI_DIRECTORY}/dev_notes/languages"
 
 DIRS=($CLI_D $NOTES_D $DEFINITIONS_D $LANGUAGES_D)
 
-for directory in $DIRS
+for directory in ${DIRS[@]}
     do
-        echo "$directory"
+        for file in "$directory"/*
+            do
+               if [ -f "$file" ];
+                    then
+                        filename=$(basename -- "$file")
+                        extension="${filename##*.}"
+                        filename="${filename%.*}"
+                        CLI_ARRAY+=( "$filename" )
+                fi
+        done
 done
-    
-for file in "$DIR"/*
-    do
-       if [ -f "$file" ];
-            then
-                filename=$(basename -- "$file")
-                extension="${filename##*.}"
-                filename="${filename%.*}"
-                CLI_ARRAY+=( "$filename" )
-        fi
-done
-# create array of dir
-# for all dir in dirs 
-    # do for file in $dir
-    #append
 
 
-FIND_DEFINITIONS() {
-    for file in "$DIR"/*
-        do
-            if [ -f "$file" ];
-                then
-                filename=$(basename -- "$file")
-                extension="${filename##*.}"
-                filename="${filename%.*}"
-                DEFINITIONS_ARRAY+=( "$filename" )
-            fi
-    done
-}
-
-FIND_LANGUAGES() {
-    for file in "$DIR"/*
-        do
-            if [ -d "$file" ];
-                then
-                filename=$(basename -- "$file")
-                extension="${filename##*.}"
-                filename="${filename%.*}"
-                LANGUAGE_ARRAY+=( "$filename" )
-            fi
-    done
-}
-
-FIND_NOTES() {
-    for file in "$DIR"/*
-        do
-            if [ -f "$file" ];
-                then
-                filename=$(basename -- "$file")
-                extension="${filename##*.}"
-                filename="${filename%.*}"
-                NOTES_ARRAY+=( "$filename" )
-           fi
-    done
-}
+echo ${CLI[@]}
+echo ${NOTES[@]}
+echo ${DEFINITIONS[@]}
+echo ${LANGUAGES[@]}
