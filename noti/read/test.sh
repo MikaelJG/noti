0c1a9a4ac659ec5830f7842358d121376816bab3
MikaelJG <mjgonsalves@protonmail.com>
2023-02-14 16:51:10 -0500

    testing for the win

--- noti/read/test.sh
+++ noti/read/test.sh
@@ -1,21 +1,17 @@
 source ~/code/noti/noti/find_.sh
-# declare -A LANGUAGES
-# LANGUAGES=( ["js"]="javascript" ["st"]="stylus" ["le"]="less" ["r"]="r" ["tm"]="tmux" ["rx"]="regex" ["sql"]="sql" ["js"]="javascript" ["re"]="react" ["ht"]="html" ["rb"]="ruby" ["sh"]="bash" ["rs"]="rust" ["ts"]="typescript" ["css"]="css" ["cs"]="csharp" ["go"]="golang" ["ph"]="php" ["py"]="python" ["ra"]="rails" ["th"]="three.js" ["vi"]="vim" ["gi"]="git" ["lu"]="lua" ["no"]="node.js" ["sa"]="sass" ["mo"]="mongodb" )
+declare -A LANGUAGES
+LANGUAGES=(["js"]="javascript" ["st"]="stylus" ["le"]="less" ["r"]="r" ["tm"]="tmux" ["rx"]="regex" ["sql"]="sql" ["js"]="javascript" ["re"]="react" ["ht"]="html" ["rb"]="ruby" ["sh"]="bash" ["rs"]="rust" ["ts"]="typescript" ["css"]="css" ["cs"]="csharp" ["go"]="golang" ["ph"]="php" ["py"]="python" ["ra"]="rails" ["th"]="three.js" ["vi"]="vim" ["gi"]="git" ["lu"]="lua" ["no"]="node.js" ["sa"]="sass" ["mo"]="mongodb")
 NOTI_D=~/code/noti
 NOTE=$1
 FILE="$NOTE.txt"
 LANGUAGE=$2
 
-CLI_T="this is cli_t, for test"
-DEFINITIONS_T="this is defi_t, for test"
-NOTES_T="this is notes_t, for test"
-
 declare -A CLI_R 
-CLI_R=(["FILE"]="$NOTI_D/dev_notes/linux/$FILE" ["ARRAY"]="$CLI_T")
+CLI_R=(["FILE"]="$NOTI_D/dev_notes/linux/$FILE")
 declare -A DEFINITION_R 
-DEFINITION_R=(["FILE"]="$NOTI_D/definition/$FILE" ["ARRAY"]="$DEFINITIONS_T")
+DEFINITION_R=(["FILE"]="$NOTI_D/definition/$FILE")
 declare -A NOTES_R 
-NOTES_R=(["FILE"]="$NOTI_D/dev_notes/languages/$LANGUAGE/$FILE" ["ARRAY"]="$NOTES_T")
+NOTES_R=(["FILE"]="$NOTI_D/dev_notes/languages/$LANGUAGE/con/$FILE")
 
 if command cat ${DEFINITION_R["FILE"]};
     then 
@@ -26,8 +22,9 @@ elif command cat ${NOTES_R["FILE"]};
 elif command cat ${CLI_R["FILE"]};
     then
         clear
-        echo "found in cli"
-        sleep  
+        echo "Got it. In cli:
+>"
+        sleep 1
         cat ${CLI_R["FILE"]}
         exit
 else
