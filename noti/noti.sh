eeb47715295e24da9cf709395e7c696f26145748
MikaelJG <mjgonsalves@protonmail.com>
2023-02-15 12:19:55 -0500

    give number of def

--- noti/noti.sh
+++ noti/noti.sh
@@ -21,13 +21,27 @@ if [[ "$1" == "-c" ]];
 
 ---------------------------------------------
      "
-# elif [[ "$1" == "-l" ]];
-#      then
-#          echo "Supported languages: "
-#          for i in "${LANGUAGE_ARRAY[@]}";
-#          do
-#              echo "$i - ${LANGUAGES[$i]}";
-#          done
+elif [[ "$1" == "-l" ]];
+     then
+         echo "Supported languages: "
+         for i in "${LANGUAGE_ARRAY[@]}";
+         do
+             echo "$i - ${LANGUAGES[$i]}";
+         done
+elif [[ "$1" == "-d" ]];
+     then
+     NUM_D=$(ls ../dev_notes/dictionary | wc -l)
+         echo "
+    Currently, your Noti has $NUM_D definitions
+         "
+
+elif [[ "$1" == "-t" ]];
+     then
+         echo "Supported languages: "
+         for i in "${LANGUAGE_ARRAY[@]}";
+         do
+             echo "$i - ${LANGUAGES[$i]}";
+         done
 else 
   ./read_.sh $1 $2
 fi
