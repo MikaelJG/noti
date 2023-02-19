8b75c001e281c25c5b0033f9c8dc1271bbf134a7
MikaelJG <mjgonsalves@protonmail.com>
2023-02-18 21:56:37 -0500

    discard output

--- noti/read_.sh
+++ noti/read_.sh
@@ -12,30 +12,28 @@ NOTES_R=(["FILE"]="$NOTES_D/languages/$LANGUAGE_OR_TOOL/$FILE")
 declare -A TOOLS_R 
 TOOLS_R=(["FILE"]="$NOTES_D/tools/$LANGUAGE_OR_TOOL/$FILE")
 
-if command cat ${DEFINITION_R["FILE"]};
-    then 
+if cat ${DEFINITION_R["FILE"]} >/dev/null 2>&1; then
         clear
-        echo "Got it. In dictionnary..."
+        echo "Got it. In dictionnary...
+>"
         sleep 1
         cat ${DEFINITION_R["FILE"]}
         exit
-elif command cat ${NOTES_R["FILE"]};
-    then
+elif cat ${NOTES_R["FILE"]} >/dev/null 2>&1; then
         clear
-        echo "Got it. In notes..."
+        echo "Got it. In notes...
+>"
         sleep 1
         cat ${NOTES_R["FILE"]}
         exit
-elif command cat ${LINUX_R["FILE"]};
-    then
+elif cat ${LINUX_R["FILE"]} >/dev/null 2>&1; then
         clear
         echo "Got it. In linux...
 >"
         sleep 1
         cat ${LINUX_R["FILE"]}
         exit
-elif command cat ${TOOLS_R["FILE"]};
-    then
+elif command cat ${TOOLS_R["FILE"]} >/dev/null 2>&1; then
         clear
         echo "Got it. In tools:
 >"
