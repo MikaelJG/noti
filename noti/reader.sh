b51b6e83494e2e285b1a965e8b9feb21212d9350
MikaelJG <mjgonsalves@protonmail.com>
2023-04-28 11:09:56 -0400

    vim open testing

--- noti/reader.sh
+++ noti/reader.sh
@@ -6,14 +6,7 @@ NOTES_D=$NOTI_DIR/notes
 NOTE=$1
 FILE="$NOTE.txt"
 LANGUAGE_OR_TOOL=$2
-
-case $3 in
-    -vo|--vo)
-        echo "in reader, in -vo"
-        sleep 10;;
-    *)
-        echo "not vo";;
-esac
+CURRENT_D=$4
 
 declare -A LINUX_R 
 LINUX_R=(["FILE"]="$NOTES_D/linux/$FILE")
@@ -24,13 +17,23 @@ NOTES_R=(["FILE"]="$NOTES_D/languages/$LANGUAGE_OR_TOOL/$FILE")
 declare -A TOOLS_R 
 TOOLS_R=(["FILE"]="$NOTES_D/tools/$LANGUAGE_OR_TOOL/$FILE")
 
+case $3 in
+    -vo|--vo)
+        cat ${DEFINITION_R["FILE"]} >/dev/null 2>&1 && cat ${DEFINITION_R["FILE"]} >> "$CURRENT_D/$FILE" && exit
+        cat ${NOTES_R["FILE"]} >/dev/null 2>&1 && cat ${NOTES_R["FILE"]} >> "$CURRENT_D/$FILE" && exit
+        cat ${LINUX_R["FILE"]} >/dev/null 2>&1 && cat ${LINUX_R["FILE"]} >> "$CURRENT_D/$FILE" && exit
+        cat ${TOOLS_R["FILE"]} >/dev/null 2>&1 && cat ${TOOLS_R["FILE"]} >> "$CURRENT_D/$FILE" && exit;;
+    *)
+        echo "not vo";;
+esac
+
 # try to cat and throw away the output
-cat ${DEFINITION_R["FILE"]} >/dev/null 2>&1 && clear && echo "Got it. In dictionary..." && sleep 1 && cat ${DEFINITION_R["FILE"]} && exit
-
-cat ${NOTES_R["FILE"]} >/dev/null 2>&1 && clear && echo "Got it. In notes..." && sleep 1 && cat ${NOTES_R["FILE"]} && exit
-
-cat ${LINUX_R["FILE"]} >/dev/null 2>&1 && clear && echo "Got it. In linux..." && sleep 1 && cat ${LINUX_R["FILE"]} && exit
-
-cat ${TOOLS_R["FILE"]} >/dev/null 2>&1 && clear && echo "Got it. In linux..." && sleep 1 && cat ${TOOLS_R["FILE"]} && exit
-
-clear && echo "Couldn't find the file"
+# cat ${DEFINITION_R["FILE"]} >/dev/null 2>&1 && ${DEFINITION_R["FILE"]} && exit
+# 
+# cat ${NOTES_R["FILE"]} >/dev/null 2>&1 && ${NOTES_R["FILE"]} && exit
+# 
+# cat ${LINUX_R["FILE"]} >/dev/null 2>&1 && cat ${LINUX_R["FILE"]} && exit
+# 
+# cat ${TOOLS_R["FILE"]} >/dev/null 2>&1 && cat ${TOOLS_R["FILE"]} && exit
+# 
+# clear && echo "Couldn't find the file"
