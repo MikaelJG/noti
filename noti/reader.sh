dbad2e7c56d6780ae17b0dea811279be3090175e
MikaelJG <mjgonsalves@protonmail.com>
2023-05-03 15:07:21 -0400

    reader refact

--- noti/reader.sh
+++ noti/reader.sh
@@ -8,30 +8,41 @@ FILE="$NOTE.txt"
 LANGUAGE_OR_TOOL=$2
 CURRENT_D=$4
 
-declare -A LINUX_R 
-LINUX_R=(["FILE"]="$NOTES_D/linux/$FILE")
-declare -A DEFINITION_R 
-DEFINITION_R=(["FILE"]="$NOTES_D/dictionary/$FILE")
-declare -A NOTES_R 
-NOTES_R=(["FILE"]="$NOTES_D/languages/$LANGUAGE_OR_TOOL/$FILE")
-declare -A TOOLS_R 
-TOOLS_R=(["FILE"]="$NOTES_D/tools/$LANGUAGE_OR_TOOL/$FILE")
+declare -A file_1 
+# Linux
+file_1=(["FILE"]="$NOTES_D/linux/$FILE")
+
+declare -A file_2 
+# Dictionary 
+file_2=(["FILE"]="$NOTES_D/dictionary/$FILE")
+
+declare -A file_3 
+# Languages 
+file_3=(["FILE"]="$NOTES_D/languages/$LANGUAGE_OR_TOOL/$FILE")
+
+declare -A file_4 
+# Tools 
+file_4=(["FILE"]="$NOTES_D/tools/$LANGUAGE_OR_TOOL/$FILE")
+
+try_cat(file) {
+    cat $file >/dev/null 2>&1
+}
 
 # last doc
 last_file=$(ls -Art | tail -n 1)
 
 case $3 in
     -vo|--vo)
-        cat ${DEFINITION_R["FILE"]} >/dev/null 2>&1 && cat ${DEFINITION_R["FILE"]} >> "$CURRENT_D/$FILE" && vim -o $last_file "$CURRENT_D/$FILE" && exit
-        cat ${NOTES_R["FILE"]} >/dev/null 2>&1 && cat ${NOTES_R["FILE"]} >> "$CURRENT_D/$FILE" && vim -o $last_file "$CURRENT_D/$FILE" && exit
-        cat ${LINUX_R["FILE"]} >/dev/null 2>&1 && cat ${LINUX_R["FILE"]} >> "$CURRENT_D/$FILE" && vim -o $last_file "$CURRENT_D/$FILE" && exit
-        cat ${TOOLS_R["FILE"]} >/dev/null 2>&1 && cat ${TOOLS_R["FILE"]} >> "$CURRENT_D/$FILE" && vim -o $last_file "$CURRENT_D/$FILE" && exit;;
+        try_cat ${file_2["FILE"]} && cat ${file_2["FILE"]} >> "$CURRENT_D/$FILE" && vim -o $last_file "$CURRENT_D/$FILE" && exit
+        cat ${file_3["FILE"]} >/dev/null 2>&1 && cat ${file_3["FILE"]} >> "$CURRENT_D/$FILE" && vim -o $last_file "$CURRENT_D/$FILE" && exit
+        cat ${file_1["FILE"]} >/dev/null 2>&1 && cat ${file_1["FILE"]} >> "$CURRENT_D/$FILE" && vim -o $last_file "$CURRENT_D/$FILE" && exit
+        cat ${file_4["FILE"]} >/dev/null 2>&1 && cat ${file_4["FILE"]} >> "$CURRENT_D/$FILE" && vim -o $last_file "$CURRENT_D/$FILE" && exit;;
 esac
 
 # try to cat and throw away the output
-cat ${DEFINITION_R["FILE"]} >/dev/null 2>&1 && clear && cat ${DEFINITION_R["FILE"]} && exit
-cat ${NOTES_R["FILE"]} >/dev/null 2>&1 && clear && cat ${NOTES_R["FILE"]} && exit
-cat ${LINUX_R["FILE"]} >/dev/null 2>&1 && clear && cat ${LINUX_R["FILE"]} && exit
-cat ${TOOLS_R["FILE"]} >/dev/null 2>&1 && clear && cat ${TOOLS_R["FILE"]} && exit
+cat ${file_2["FILE"]} >/dev/null 2>&1 && clear && cat ${file_2["FILE"]} && exit
+cat ${file_3["FILE"]} >/dev/null 2>&1 && clear && cat ${file_3["FILE"]} && exit
+cat ${file_1["FILE"]} >/dev/null 2>&1 && clear && cat ${file_1["FILE"]} && exit
+cat ${file_4["FILE"]} >/dev/null 2>&1 && clear && cat ${file_4["FILE"]} && exit
 
 clear && echo "Couldn't find the file"
