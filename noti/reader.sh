116f5a3386d7966779d08e2fab76bf3a86be7a46
MikaelJG <mjgonsalves@protonmail.com>
2023-05-25 13:39:55 -0400

    reader double quotes

--- noti/reader.sh
+++ noti/reader.sh
@@ -1,6 +1,6 @@
 SCRIPT=${BASH_SOURCE[0]}
-DIR=$(dirname $SCRIPT)
-NOTI_DIR=$(dirname $DIR)
+DIR=$(dirname "$SCRIPT")
+NOTI_DIR=$(dirname "$DIR")
 
 NOTES_D=$NOTI_DIR/notes
 NOTE=$1
@@ -28,26 +28,26 @@ last_file=$(ls -Art | tail -n 1)
 
 try_cat() {
     file=$1
-    cat $file >/dev/null 2>&1 && clear
+    cat "$file" >/dev/null 2>&1 && clear
 }
 
 open_last_file() {
     # last doc
-    vim -o $last_file "$CURRENT_D/$FILE"
+    vim -o "$last_file" "$CURRENT_D/$FILE"
 }
 
 case $3 in
     -vo|--vo)
-        try_cat ${file_2["FILE"]} && cat ${file_2["FILE"]} >> "$CURRENT_D/$FILE" && open_last_file && exit
-        try_cat ${file_3["FILE"]} && cat ${file_3["FILE"]} >> "$CURRENT_D/$FILE" && open_last_file && exit
-        try_cat ${file_1["FILE"]} && cat ${file_1["FILE"]} >> "$CURRENT_D/$FILE" && open_last_file && exit
-        try_cat ${file_4["FILE"]} && cat ${file_4["FILE"]} >> "$CURRENT_D/$FILE" && open_last_file && exit ;;
+        try_cat "${file_2["FILE"]}" && cat "${file_2["FILE"]}" >> "$CURRENT_D/$FILE" && open_last_file && exit
+        try_cat "${file_3["FILE"]}" && cat "${file_3["FILE"]}" >> "$CURRENT_D/$FILE" && open_last_file && exit
+        try_cat "${file_1["FILE"]}" && cat "${file_1["FILE"]}" >> "$CURRENT_D/$FILE" && open_last_file && exit
+        try_cat "${file_4["FILE"]}" && cat "${file_4["FILE"]}" >> "$CURRENT_D/$FILE" && open_last_file && exit ;;
 esac
 
 # try to cat and throw away the output
-try_cat ${file_2["FILE"]} && cat ${file_2["FILE"]} && exit
-try_cat ${file_3["FILE"]} && cat ${file_3["FILE"]} && exit
-try_cat ${file_1["FILE"]} && cat ${file_1["FILE"]} && exit
-try_cat ${file_4["FILE"]} && cat ${file_4["FILE"]} && exit
+try_cat "${file_2["FILE"]}" && cat "${file_2["FILE"]}" && exit
+try_cat "${file_3["FILE"]}" && cat "${file_3["FILE"]}" && exit
+try_cat "${file_1["FILE"]}" && cat "${file_1["FILE"]}" && exit
+try_cat "${file_4["FILE"]}" && cat "${file_4["FILE"]}" && exit
 
 clear && echo "Couldn't find the file"
