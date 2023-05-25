8c9d1dce23af7fea0aa30f6526eb86cce79d181a
MikaelJG <mjgonsalves@protonmail.com>
2023-05-25 13:36:32 -0400

    double quotes

--- noti/noti.sh
+++ noti/noti.sh
@@ -23,7 +23,7 @@ WRITER_PATH=$UTILS_DIR/writer.sh
 
 # empty ARGS is given "--help"
 
-if [ -z "$1" ]; then
+if [ -z ""$1"" ]; then
     ARGS=("--help")
 else
     ARGS=("$@")
@@ -45,7 +45,7 @@ Options:
         -wx,        Write a new note in Linux
     -x, count lin   Count your number of notes in Linux
 
-   -- in progress -- noti rs ls : list all notes in rust
+   -- in progress -- noti rs find : list all notes in rust
 EOF
 ;;
     -a|--a)
@@ -71,27 +71,27 @@ EOF
 EOF
 ;;
    -d|--d)
-        NUM_D=$(ls ${DICTIONARY_PATH} | wc -l)
+        NUM_D=$(find "${DICTIONARY_PATH}" | wc -l)
         echo -e "\nCurrently, your Noti has $NUM_D definitions!\n";;
     -e|--e)
-        [[ $# -eq 3 ]] && $EDITOR_PATH $3 $2 || $EDITOR_PATH $2;;
+        [[ $# -eq 3 ]] && "$EDITOR_PATH" "$3" "$2" || $EDITOR_PATH "$2";;
     -wd|--wd|-wl|--wl|-wx|--wx|-wt|--wt)
-        [[ $# -eq 3 ]] && $WRITER_PATH $1 $3 $2 || $WRITER_PATH $1 $2;;
+        [[ $# -eq 3 ]] && $WRITER_PATH "$1" "$3" "$2" || $WRITER_PATH "$1" "$2";;
     -l|--l)
-        LANGUAGES=$(ls ${LANGUAGES_PATH})
+        LANGUAGES=$(find "${LANGUAGES_PATH}")
         echo -e "\nCurrently, your Noti supports:\n$LANGUAGES";;
     -t|--t)
-        TOOLS_N=$(ls ${TOOLS_PATH})
+        TOOLS_N=$(find "${TOOLS_PATH}")
         echo -e "\nCurrently, your Noti supports:\n$TOOLS_N";;
     -x|--x)
-        NUM_LINUX_N=$(ls ${LINUX_PATH} | wc -l)
+        NUM_LINUX_N=$(find "${LINUX_PATH}" | wc -l)
         echo -e "\nCurrently, your Noti has $NUM_LINUX_N notes on Linux!\n";;
     -vo|--vo)
-        # noti -vo sh $1 >> $1.txt && vo (last_file) $1.txt
+        # noti -vo sh "$1" >> "$1".txt && vo (last_file) "$1".txt
         # alias nvo="noti vo"
-        # nvo sh $1 >> $1.txt && vo (last_file) $1.txt
-        [[ " $2 " -eq 0 ]] && [[ " $3 " -eq 0 ]] && $READER_PATH $3 $2 $1 $PWD;;
+        # nvo sh "$1" >> "$1".txt && vo (last_file) "$1".txt
+        [[ " $2 " -eq 0 ]] && [[ " $3 " -eq 0 ]] && $READER_PATH "$3" "$2" "$1" "$PWD";;
     *)
-        [[ " $2 " -eq 0 ]] && $READER_PATH $2 $1 || $READER_PATH $1;;
+        [[ " $2 " -eq 0 ]] && $READER_PATH "$2" "$1" || $READER_PATH "$1";;
 esac
 
