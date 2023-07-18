9b289a5ea24d85f77e691147e04dd081d74d5b6c
MikaelJG <mjgonsalves@protonmail.com>
2023-07-18 12:10:58 -0400

    update

--- noti/update.sh
+++ noti/update.sh
@@ -10,10 +10,15 @@ case $arg_length in
     1)
         note_name=${ARGS[0]}
         echo "one argument give to update.sh"
-        echo "This $note_name, is the name of the note. Correct?";;
-        # read [y/yes, n/no]
+        echo "This $note_name, is the name of the note? [y/yes, n/no] >"
+        # read answer
         
         # Try all places with regular notes
+        if cat "$HOME/code/noti_me/notes/dictionary/$note_name" 2>&1 /dev/null; then
+            echo "Note found in output directory: $HOME/code/noti_me/notes/dictionary."
+            echo "Update file? [y/yes, n/no] >"
+            # read answer
+        fi;;
 
     2)
         language=${ARGS[0]}
@@ -21,8 +26,8 @@ case $arg_length in
 
         echo "two arguments given to update.sh"
         echo "This $note_name, should be the name of the note."
-        echo "This $language, should be the language. Correct?";;
-        # read [y/yes, n/no]
+        echo "This $language, is the language? [y/yes, n/no] >"
+        # read answer
 esac
 
 
