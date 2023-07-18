368d321e61cfdec79ae63f50c951c732b5caca5a
MikaelJG <mjgonsalves@protonmail.com>
2023-07-18 13:15:23 -0400

    testing the readable noti

---
+++ noti/test.sh
@@ -0,0 +1,32 @@
+./noti.sh -e sh if that
+echo "./noti.sh -e sh if that"
+./noti.sh -e sh if
+echo "./noti.sh -e sh if"
+./noti.sh -e sh
+echo "./noti.sh -e sh"
+./noti.sh -e if
+echo "./noti.sh -e if"
+./noti.sh -w sh if that
+echo "./noti.sh -w sh if that"
+./noti.sh -w sh if
+echo "./noti.sh -w sh if"
+./noti.sh -w sh
+echo "./noti.sh -w sh"
+./noti.sh -w if
+echo "./noti.sh -w if"
+./noti.sh -vo sh if that
+echo "./noti.sh -vo sh if that"
+./noti.sh -vo sh if
+echo "./noti.sh -vo sh if"
+./noti.sh -vo sh
+echo "./noti.sh -vo sh"
+./noti.sh -vo if
+echo "./noti.sh -vo if"
+./noti.sh sh if that
+echo "./noti.sh sh if that"
+./noti.sh sh if
+echo "./noti.sh sh if"
+./noti.sh sh
+echo "./noti.sh sh"
+./noti.sh if
+echo "./noti.sh if"
