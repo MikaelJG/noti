750223f9189bd9afc469b8ce2de5bd931a3a90a4
MikaelJG <mjgonsalves@protonmail.com>
2023-07-25 16:51:02 -0400

    trying keywords ideas

--- noti/keyword_feature/keywords.cpp
+++ noti/keyword_feature/keywords.cpp
@@ -5,15 +5,48 @@
 
 int main() {
 
+    // all of these should open if.cpp
+    //                          if, else, elsif, else if
+    //
+    // all of these should open iterate.cpp
+    //                          iterate, iterator, iterating, iterators
+    
+    // {
+    //      else: if.cpp,
+    //      elsif: if.cpp,
+    //      else if: if.cpp,
+    //      if: if.cpp,
+    //
+    //      iterate: iterate.cpp,
+    //      iterator: iterate.cpp,
+    //      iterators: iterate.cpp,
+    //      iterating: iterate.cpp,
+    // }
+    //
+
     std::array<std::string, 4> if_array{"if", "else", "elsif", "else if"};
-    std::array<std::string, 4> iterate_array{"iterate", "iterator", "iterating", "interators"};
+    std::array<std::string, 4> iterate_array{"iterate", "iterator", "iterating", "iterators"};
 
 
     std::unordered_map<std::string, std::string> keywords;
 
-    keywords["if"] = "path/to/if.cpp";
-    
 
+    for (size_t i = 0; i < if_array.size(); i++) {
+
+        std::string keyword = if_array[i];
+
+        keywords[keyword] = "path/to/if.cpp";
+
+    }
+
+    for (size_t i = 0; i < iterate_array.size(); i++) {
+
+        std::string keyword = iterate_array[i];
+
+        keywords[keyword] = "path/to/iterate.cpp";
+
+    }
+    
     for (const auto& k : keywords) {
         std::cout << "Key: " << k.first << ", Path: " << k.second << '\n';
     }
